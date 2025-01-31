0869d2b2f08dbcaff5314670d47452a74cc65836
AlfinMoh24 <alfinmoh24@gmail.com>
2025-02-01 01:52:22 +0700

    foto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/storage/ktp/{filename}', function ($filename) {
+    // Path ke file yang ada di storage/app/public/
+    $path = storage_path("app/public/ktp/{$filename}");
+
+    // Cek apakah file ada
+    if (file_exists($path)) {
+        // Mengembalikan file dengan header yang sesuai
+        return Response::make(file_get_contents($path), 200, [
+            'Content-Type' => mime_content_type($path),
+            'Content-Disposition' => 'inline; filename="' . $filename . '"',
+        ]);
+    }
+
+    // Jika file tidak ditemukan
+    abort(404);
 });
